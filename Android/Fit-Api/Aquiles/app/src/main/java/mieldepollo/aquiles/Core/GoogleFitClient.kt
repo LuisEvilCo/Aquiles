31c27f8957c55672f820c46918bd0ab6db2ed37c
Luis Tinajero Muñozcano <ltm150895@gmail.com>
2017-06-12 21:40:27 -0500

    Subscribe to things , steps for example

--- Android/Fit-Api/Aquiles/app/src/main/java/mieldepollo/aquiles/Core/GoogleFitClient.kt
+++ Android/Fit-Api/Aquiles/app/src/main/java/mieldepollo/aquiles/Core/GoogleFitClient.kt
@@ -10,7 +10,6 @@ import android.os.Bundle
 import android.util.Log
 import com.google.android.gms.common.api.ResultCallback
 import com.google.android.gms.common.api.Status
-import com.google.android.gms.fitness.data.DataPoint
 import com.google.android.gms.fitness.data.DataSource
 import com.google.android.gms.fitness.result.DataSourcesResult
 import com.google.android.gms.fitness.data.DataType
@@ -18,6 +17,12 @@ import com.google.android.gms.fitness.request.DataSourcesRequest
 import com.google.android.gms.fitness.request.OnDataPointListener
 import com.google.android.gms.fitness.request.SensorRequest
 import java.util.concurrent.TimeUnit
+import android.content.ContentValues.TAG
+import com.google.android.gms.fitness.FitnessStatusCodes
+
+
+
+//https://github.com/googlesamples/android-fit/blob/master/StepCounter/app/src/main/java/com/google/android/gms/fit/samples/stepcounter/MainActivity.java
 
 
 val TagFitClient = "GoogleFitClient"
@@ -33,12 +38,18 @@ val TagFitClient = "GoogleFitClient"
 fun buildFitnessClient(mainActivity: MainActivity): GoogleApiClient{
     return GoogleApiClient.Builder(mainActivity)
             .addApi(Fitness.SENSORS_API)
+            .addApi(Fitness.RECORDING_API)
+            .addApi(Fitness.HISTORY_API)
             .addScope(Scope(Scopes.FITNESS_LOCATION_READ))
+            .addScope(Scope(Scopes.FITNESS_BODY_READ_WRITE))
+            .addScope(Scope(Scopes.FITNESS_ACTIVITY_READ_WRITE))
+            .addScope(Scope(Scopes.FITNESS_NUTRITION_READ_WRITE))
             .addConnectionCallbacks(
                     object : GoogleApiClient.ConnectionCallbacks {
                         override fun onConnected(p0: Bundle?) {
                             Log.i(TagFitClient, "Connected !!!")
-                            findFitnessDataSources(mainActivity = mainActivity)
+                            findFitnessDataSource(mainActivity = mainActivity)
+                            subscribeToStepCounter(mainActivity = mainActivity)
                         }
 
                         override fun onConnectionSuspended(p0: Int) {
@@ -67,32 +78,51 @@ fun buildFitnessClient(mainActivity: MainActivity): GoogleApiClient{
  *     #register(GoogleApiClient, SensorRequest, DataSourceListener)},
  * where the {@link SensorRequest} contains the desired data type.
  */
-fun findFitnessDataSources(mainActivity: MainActivity){
-    if(checkPermissions(mainActivity))
-    mainActivity.mClient.let { client ->
-        // Note: Fitness.SensorsApi.findDataSources() requires the ACCESS_FINE_LOCATION permission.
-        Fitness.SensorsApi.findDataSources(client, DataSourcesRequest.Builder()
-                // At least one dataType must be specified.
-                .setDataTypes(DataType.TYPE_LOCATION_SAMPLE)
-                // Can specify whether data type is raw or derived.
-                .setDataSourceTypes(DataSource.TYPE_RAW)
-                .build())
-                .setResultCallback {
-                    ResultCallback<DataSourcesResult> { dataSourcesResult ->
-                        Log.i(TagFitClient, "Result " + dataSourcesResult.status.toString())
-                        dataSourcesResult.dataSources.forEach { dataSource ->
-                            Log.i(TagFitClient, "Result: " + dataSource.toString())
-                            Log.i(TagFitClient, "Data Source Type: " + dataSource.dataType.name)
-                            if(dataSource.dataType == DataType.TYPE_LOCATION_SAMPLE){
-                                mainActivity.mListener ?: registerFitnessDataListener(
-                                        dataSource = dataSource,
-                                        dataType = DataType.TYPE_LOCATION_SAMPLE,
-                                        mainActivity = mainActivity)
-                            }
-                        }
+
+private fun findFitnessDataSource(mainActivity: MainActivity) {
+    // [START find_data_sources]
+    // Note: Fitness.SensorsApi.findDataSources() requires the ACCESS_FINE_LOCATION permission.
+    Fitness.SensorsApi.findDataSources(mainActivity.mClient, DataSourcesRequest.Builder()
+            // At least one datatype must be specified.
+            .setDataTypes(DataType.TYPE_LOCATION_SAMPLE)
+            // Can specify whether data type is raw or derived.
+            .setDataSourceTypes(DataSource.TYPE_RAW)
+            .build())
+            .setResultCallback { dataSourcesResult ->
+                Log.i(TagFitClient, "Result: " + dataSourcesResult.status.toString())
+                for (dataSource in dataSourcesResult.dataSources) {
+                    Log.i(TagFitClient, "Data source found: " + dataSource.toString())
+                    Log.i(TagFitClient, "Data Source type: " + dataSource.dataType.name)
+
+                    //Let's register a listener to receive Activity data!
+                    if (dataSource.dataType == DataType.TYPE_LOCATION_SAMPLE && mainActivity.mListener == null) {
+                        Log.i(TagFitClient, "Data source for LOCATION_SAMPLE found!  Registering.")
+                        //registerFitnessDataListener(dataSource, DataType.TYPE_LOCATION_SAMPLE)
                     }
                 }
-    }
+            }
+    // [END find_data_sources]
+}
+
+/**
+ * Record step data by requesting a subscription to background step data.
+ */
+private fun subscribeToStepCounter(mainActivity: MainActivity) {
+    // To create a subscription, invoke the Recording API. As soon as the subscription is
+    // active, fitness data will start recording.
+    Fitness.RecordingApi.subscribe(mainActivity.mClient, DataType.TYPE_STEP_COUNT_CUMULATIVE)
+            .setResultCallback { status ->
+                if (status.isSuccess) {
+                    if (status.statusCode == FitnessStatusCodes.SUCCESS_ALREADY_SUBSCRIBED) {
+                        Log.i(TagFitClient, "Existing subscription for activity detected.")
+                    } else {
+                        Log.i(TagFitClient, "Successfully subscribed!")
+                    }
+                } else {
+                    Log.w(TagFitClient, "There was a problem subscribing."
+                    )
+                }
+            }
 }
 
 /**
@@ -100,7 +130,7 @@ fun findFitnessDataSources(mainActivity: MainActivity){
  * {@link DataType} combo.
  */
 private fun registerFitnessDataListener(dataSource: DataSource, dataType: DataType, mainActivity: MainActivity){
-    mainActivity.mListener = OnDataPointListener {dataPoint ->
+    mainActivity.mListener = OnDataPointListener { dataPoint ->
         dataPoint.dataType.fields.forEach{ field ->
             Log.i(TagFitClient, "Detected DataPoint field: " + field.name)
             Log.i(TagFitClient, "Detected DataPoint value : " + dataPoint.getValue(field))
